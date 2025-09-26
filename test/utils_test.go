32fa65f1a3d788c1663b8b507b4ad74d39b88157
Gianluigi <gianluigi.provinciali@gmail.com>
2025-09-26 16:40:45 +0200

    feat: add maxRetry to ContinousRetry and make sleep optional

---
+++ test/utils_test.go
@@ -0,0 +1,38 @@
+package test
+
+import (
+	"context"
+	"errors"
+	"provincialig/golimitless/utils"
+	"testing"
+)
+
+func TestMaxRetrySuccess(t *testing.T) {
+	counter := 0
+
+	err := utils.ContinousRetry(context.Background(), 0, 0, func() error {
+		if counter == 5 {
+			return nil
+		}
+
+		counter++
+		return errors.New("")
+	})
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if counter != 5 {
+		t.Fatal(counter)
+	}
+}
+
+func TestMaxRetryFail(t *testing.T) {
+	err := utils.ContinousRetry(context.Background(), 0, 10, func() error {
+		return errors.New("")
+	})
+	if err == nil {
+		t.Fail()
+	}
+}
--- utils/utils.go
+++ utils/utils.go
@@ -2,12 +2,13 @@ package utils
 
 import (
 	"context"
+	"errors"
 	"sync"
 	"time"
 )
 
-// Joins multiple channels into one.
-// The return channel will be closed when all original channels are closed.
+// JoinChannels unisce più channel in un unico.
+// Il channel di ritorno verrà chiuso in automatico quando tutti i channel iniziali saranno chiusi.
 //
 // Default buffer size: 1000
 func JoinChannels[T any](channels []<-chan T, bufferSize ...int) <-chan T {
@@ -18,7 +19,7 @@ func JoinChannels[T any](channels []<-chan T, bufferSize ...int) <-chan T {
 
 	var (
 		wg  sync.WaitGroup
-		out chan T = make(chan T, buff)
+		out = make(chan T, buff)
 	)
 
 	go func() {
@@ -39,20 +40,39 @@ func JoinChannels[T any](channels []<-chan T, bufferSize ...int) <-chan T {
 	return out
 }
 
-func ContinousRetry(ctx context.Context, sleep time.Duration, fn func() error) {
+var ErrMaxRetry = errors.New("max retry")
+
+// ContinuousRetry esegue fn fino al successo (non ritorna più errore) o alla cancellazione del context.
+//
+// sleep: durata di attesa tra i tentativi (0 = nessuna attesa)
+//
+// maxRetry: numero massimo di tentativi (0 = illimitato)
+func ContinousRetry(ctx context.Context, sleep time.Duration, maxRetry int, fn func() error) error {
+	retry := 0
+
 	for {
 		if ctx.Err() != nil {
-			return
+			return nil
 		}
 
+		if maxRetry > 0 && retry >= maxRetry {
+			return ErrMaxRetry
+		}
+
+		retry++
+
 		if err := fn(); err != nil {
+			if sleep == 0 {
+				continue
+			}
+
 			select {
 			case <-time.After(sleep):
 			case <-ctx.Done():
-				return
+				return nil
 			}
 		} else {
-			return
+			return nil
 		}
 	}
 }
