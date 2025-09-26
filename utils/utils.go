c071b276fed470f8782019306a0d1f9b9f6cac57
Gianluigi <gianluigi.provinciali@gmail.com>
2025-09-26 16:07:48 +0200

    fat: start to add tools doc

--- utils/utils.go
+++ utils/utils.go
@@ -6,7 +6,11 @@ import (
 	"time"
 )
 
-func JoinChannels[T any](subscriptions []<-chan T, bufferSize ...int) <-chan T {
+// Joins multiple channels into one.
+// The return channel will be closed when all original channels are closed.
+//
+// Default buffer size: 1000
+func JoinChannels[T any](channels []<-chan T, bufferSize ...int) <-chan T {
 	var buff = 1000
 	if len(bufferSize) > 0 {
 		buff = bufferSize[0]
@@ -18,7 +22,7 @@ func JoinChannels[T any](subscriptions []<-chan T, bufferSize ...int) <-chan T {
 	)
 
 	go func() {
-		for _, ch := range subscriptions {
+		for _, ch := range channels {
 			wg.Add(1)
 			go func(c <-chan T, wg *sync.WaitGroup) {
 				for val := range c {
