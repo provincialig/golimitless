ed1dc8f5d3e190b7b6852b6a9140ebc0f4dde86a
Gianluigi <gianluigi.provinciali@gmail.com>
2025-09-26 15:45:20 +0200

    feat: add slice forEach and Reduce

--- utils/slice.go
+++ utils/slice.go
@@ -1,9 +1,9 @@
 package utils
 
-func SliceFilter[T any](arr []T, fn func(el T) bool) []T {
+func SliceFilter[T any](silce []T, fn func(el T) bool) []T {
 	result := []T{}
 
-	for _, el := range arr {
+	for _, el := range silce {
 		if fn(el) {
 			result = append(result, el)
 		}
@@ -12,10 +12,10 @@ func SliceFilter[T any](arr []T, fn func(el T) bool) []T {
 	return result
 }
 
-func SliceMap[T any, K any](arr []T, fn func(el T) (K, error)) ([]K, error) {
+func SliceMap[T any, K any](slice []T, fn func(el T) (K, error)) ([]K, error) {
 	result := []K{}
 
-	for _, el := range arr {
+	for _, el := range slice {
 		r, err := fn(el)
 		if err != nil {
 			return nil, err
@@ -25,4 +25,24 @@ func SliceMap[T any, K any](arr []T, fn func(el T) (K, error)) ([]K, error) {
 	}
 
 	return result, nil
+}
+
+func SliceForEach[T any](slice []T, fn func(el T)) {
+	for _, el := range slice {
+		fn(el)
+	}
+}
+
+func SliceReduce[T any, K any](slice []T, initial K, fn func(acc K, el T) (K, error)) (K, error) {
+	acc := initial
+
+	for _, el := range slice {
+		v, err := fn(acc, el)
+		if err != nil {
+			return acc, err
+		}
+		acc = v
+	}
+
+	return acc, nil
 }
