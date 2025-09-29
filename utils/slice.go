a460a1eac4c1879642cf73b3eb396439de6652c4
Gianluigi <gianluigi.provinciali@gmail.com>
2025-09-29 20:00:15 +0200

    feat: add MapToSlice slice utils

--- utils/slice.go
+++ utils/slice.go
@@ -48,3 +48,31 @@ func SliceReduce[T any, K any](slice []T, initial K, fn func(acc K, el T) (K, er
 
 	return acc, nil
 }
+
+type SliceItem[T any, K any] struct {
+	Index T
+	Value K
+}
+
+func MapToSlice[T comparable, K any](m map[T]K) []SliceItem[T, K] {
+	res := []SliceItem[T, K]{}
+
+	for k, v := range m {
+		res = append(res, SliceItem[T, K]{
+			Index: k,
+			Value: v,
+		})
+	}
+
+	return res
+}
+
+func SliceToMap[T comparable, K any](slice []SliceItem[T, K]) map[T]K {
+	res := map[T]K{}
+
+	for _, el := range slice {
+		res[el.Index] = el.Value
+	}
+
+	return res
+}
