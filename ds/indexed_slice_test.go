5eb5df7ed27b1f903a2511f68092bc445b261be7
Gianluigi Provinciali <gianluigi.provinciali@gmail.com>
2025-09-30 11:49:35 +0200

    chore: improve IndexedSlice test

--- ds/indexed_slice_test.go
+++ ds/indexed_slice_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestIndexedSlice(t *testing.T) {
-	t.Run("Has", func(t *testing.T) {
+	t.Run("Get", func(t *testing.T) {
 		s := ds.NewIndexedSlice[int, string]()
 
 		s.Append(1, "ciao")
@@ -22,6 +22,61 @@ func TestIndexedSlice(t *testing.T) {
 		res := strings.Join(v, "")
 		if res != "ciao mondo" {
 			t.Fatalf("Result must be: ciao mondo: %s", res)
+		}
+	})
+
+	t.Run("Has", func(t *testing.T) {
+		s := ds.NewIndexedSlice[int, int]()
+
+		s.Append(1, 1)
+		s.Append(2, 1)
+		s.Append(1, 2)
+
+		if !s.Has(1) {
+			t.Fatal("Slice must be contain index 1")
+		}
+
+		if !s.Has(2) {
+			t.Fatal("Slice must be contain index 2")
+		}
+
+		if s.Has(4) {
+			t.Fatal("Slice must not be contain index 4")
+		}
+	})
+
+	t.Run("Remove", func(t *testing.T) {
+		s := ds.NewIndexedSlice[int, int]()
+		s.Append(1, 1)
+		s.Append(1, 2)
+		s.Append(1, 3)
+
+		s.Remove(2, 6)
+		s.Remove(1, 0)
+
+		v, ok := s.Get(1)
+		if !ok {
+			t.Fatal("Slice must be contain index 1")
+		}
+
+		size := len(v)
+		if size != 2 {
+			t.Fatalf("Size of index 1 must be 2: %d", size)
+		}
+	})
+
+	t.Run("Contains", func(t *testing.T) {
+		s := ds.NewIndexedSlice[int, int]()
+		s.Append(1, 1)
+		s.Append(1, 2)
+		s.Append(1, 3)
+
+		if !s.Contains(1, 2) {
+			t.Fatal("Index 1 must be contains element 2")
+		}
+
+		if s.Contains(2, 2) {
+			t.Fatal("Index 2 must be not contains element 2")
 		}
 	})
 }
